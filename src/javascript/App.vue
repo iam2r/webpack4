dc7aeb409f86f222bee7d4f90a043d82688515ed
Your Name <you@example.com>
2018-07-11 16:59:46 +0800

    缩放demo

--- src/javascript/App.vue
+++ src/javascript/App.vue
@@ -1,11 +1,14 @@
 <template>
-    <div id="app">
-        <transition :name="'slide-left'">
-            <!-- <keep-alive :include="['branddetail','search','reviewdetail']"> -->
-            <router-view class="router"></router-view>
-     
-        </transition>
+  <div id="app">
+    <transition :name="'slide-left'">
+      <!-- <keep-alive :include="['branddetail','search','reviewdetail']"> -->
+      <router-view class="router"></router-view>
+
+    </transition>
+    <div class="game" :class="isLandscape?'landscape':''">
+      {{innerWidth+'/'+innerHeight}} {{width}}
     </div>
+  </div>
 </template>
 
 <script>
@@ -13,12 +16,30 @@ export default {
   name: "App",
   data() {
     return {
-      // transitionName: "slide-left"
+      // transitionName: "slide-left",
+      width: window.innerWidth,
+      height: window.innerHeight
     };
   },
   created() {},
-  mounted() {},
-  computed: {},
+  mounted() {
+    var that = this;
+    window.onresize = function() {
+      that.width = window.innerWidth;
+      that.height = window.innerHeight;
+    };
+  },
+  computed: {
+    isLandscape() {
+      return this.width / this.height >= 1065 / 500;
+    },
+    innerWidth() {
+      return window.innerWidth;
+    },
+    innerHeight() {
+      return window.innerHeight;
+    }
+  },
   methods: {},
   updated() {}
 };
@@ -36,6 +57,19 @@ export default {
     display: flex;
     align-items: center;
     justify-content: center;
+  }
+}
+.game {
+  position: absolute;
+  background: red;
+  top: 50%;
+  left: 50%;
+  height: calc(100vw * 600 / 1065);
+  width: 100vw;
+  transform: translate(-50%, -50%);
+  &.landscape {
+    height: 100vh;
+    width: calc(100vh * 1065 / 600);
   }
 }
 </style>
